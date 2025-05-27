d52ccacc2d6f43fb78605888b15097bdaf2a0641
Ezequiel Cordero <eze.cordero54@gmail.com>
2025-05-27 16:53:25 -0400

    added a UI

--- task-frontend/vite.config.js
+++ task-frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // default Vite port
+    proxy: {
+      '/api': 'http://localhost:8080' // proxies /api calls to Spring Boot backend
+    }
+  }
 })
